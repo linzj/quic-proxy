1ae6c4342b78375d337cb867825e0f55532ac9c7
linzj <zuojian.lzj@alibaba-inc.com>
2020-02-18 09:31:59 +0800

    max idle time to 1 hour.

--- qpserver/main.go
+++ qpserver/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/elazarl/goproxy"
 	"github.com/lucas-clemente/quic-go"
@@ -42,7 +43,7 @@ func main() {
 	}
 	username, password := parts[0], parts[1]
 
-	listener, err := quic.ListenAddr(listenAddr, generateTLSConfig(cert, key), nil)
+	listener, err := quic.ListenAddr(listenAddr, generateTLSConfig(cert, key), &quic.Config{MaxIdleTimeout: time.Hour})
 	if err != nil {
 		log.Error("listen failed:%v", err)
 		return
